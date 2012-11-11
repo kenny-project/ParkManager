db1bf3b2bd26a2f5a2f048e8a807b81b84a5eaef
kenny <software@PC094>
2012-11-11 16:34:31 +0800

    添加车的概念，并添加7个案例

--- src/com/kenny/park/test/test.java
+++ src/com/kenny/park/test/test.java
@@ -3,9 +3,9 @@ package com.kenny.park.test;
 import junit.framework.Assert;
 
 import org.junit.Test;
-import org.junit.experimental.theories.internal.Assignments;
 
 import com.kenny.park.ParkManager;
+import com.kenny.park.bean.CarBean;
 
 public class test 
 {
@@ -34,6 +34,9 @@ public class test
 		Assert.assertTrue(manager.bFull());
 		result=manager.PushCar(5000,new Object());
 		Assert.assertEquals(0, result);
+		
+		
+		
 	}
 	//取车
 	@Test
@@ -46,6 +49,34 @@ public class test
 		Object obj=manager.PopCar(1);
 		Assert.assertNotNull(obj);		
 		Assert.assertEquals(ParkManager.MaxCount, manager.Display());
+		
+		//case 4//一个空停车场取车
+		manager.Clear();
+		obj=manager.PopCar(1);
+		Assert.assertNull(obj);//空
+		
+		//case 5//一个有效停车凭证取车 取出原来的车
+		manager.Clear();
+		CarBean car= new CarBean();
+		result=	manager.PushCar(1, car);
+		Assert.assertEquals(ParkManager.MaxCount-1, manager.Display());
+		obj=manager.PopCar(1);
+		Assert.assertEquals(car,obj);		
+
+		//case 6 无效停车证。取不出车
+		manager.Clear();
+		obj=manager.PopCar(100);
+		Assert.assertNull(obj);		
+		
+		//case 7有效停车证，取两次，第一次成功，第二次失败
+		manager.Clear();
+		result=	manager.PushCar(1, car);
+		Assert.assertEquals(ParkManager.MaxCount-1, manager.Display());
+		obj=manager.PopCar(1);
+		Assert.assertNotNull(obj);	
+		obj=manager.PopCar(1);
+		Assert.assertNull(obj);
+		
 		
 //		ParkManager manager=new ParkManager();
 //		for(int i=0;i<manager.Display();i++)
